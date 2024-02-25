bef3499f57ffdaac34c8762c8e46560d502ba02a
Teavern <teavern.draws@gmail.com>
2024-02-25 10:52:57 -0500

    Site updates

--- src/Gallery/ArtCard.js
+++ src/Gallery/ArtCard.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col'
+import {React, useState} from 'react'
+import Modal from 'react-bootstrap/Modal';
+import Image from 'react-bootstrap/Image';
+
 /**
  * url -- relative path of the URL to pull from
  */
 function ArtCard(props) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
-            <a href={props.url} target='_blank'><img class='artcard' src={props.url}></img></a>
+        <>
+            <a href="#"><img class='artcard' src={props.url} onClick={handleShow} /></a>
+
+            <Modal show={show} onHide={handleClose} size="lg">
+                <Modal.Body>
+                    <Image class='artmodal' src={props.url} onClick={handleClose} fluid/>
+                </Modal.Body>
+            </Modal>
+        </>
     )
 }
 
